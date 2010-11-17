8f494106c4d1597baf1acdb835aeb8448c9790ca
Joakim Plate <joakim.plate@qrtech.se>
2010-11-17 11:45:49 +0000

    fixed GET_UINT16/32 used invalid offsets
    git-svn-id: https://svn.qrtech.se/svn/Sources/SM10002/trunk@23680 6bce8083-c9ef-d345-8e70-90becfb1c772

--- source/Xcp_ByteStream.h
+++ source/Xcp_ByteStream.h
@@ -20,8 +20,8 @@
 
 
 #define GET_UINT8(data, offset)  (*((uint8* )(data)+(offset)))
-#define GET_UINT16(data, offset) (*((uint16*)(data)+(offset)))
-#define GET_UINT32(data, offset) (*((uint32*)(data)+(offset)))
+#define GET_UINT16(data, offset) (*(uint16*)((uint8*)(data)+(offset)))
+#define GET_UINT32(data, offset) (*(uint16*)((uint8*)(data)+(offset)))
 
 
 #define SET_UINT8(data, offset, value) do {                 \
