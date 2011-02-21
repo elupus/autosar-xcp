94c036a564713df651c619d795e0eb7cb3b3bf18
Joakim Plate <joakim.plate@qrtech.se>
2011-02-21 09:52:45 +0000

    changed: split Xcp_Cbk.h into separate files for CAN/ETH
    git-svn-id: https://svn.qrtech.se/svn/Sources/SM10002/trunk@23828 6bce8083-c9ef-d345-8e70-90becfb1c772

--- source/XcpOnCan_Cbk.h
+++ source/XcpOnCan_Cbk.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010 Joakim Plate
+/* Copyright (C) 2011 Joakim Plate
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,22 +15,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef XCP_CBK_H_
-#define XCP_CBK_H_
+#ifndef XCPONCAN_CBK_H_
+#define XCPONCAN_CBK_H_
 
 #include "ComStack_Types.h"
 #include "CanIf.h"
 
-/* CAN interface callbacks */
 void           Xcp_CanRxIndication   (PduIdType XcpRxPduId, PduInfoType* XcpRxPduPtr);
 void           Xcp_CanRxSpecial      (PduIdType XcpRxPduId, const uint8 * data, uint8 len, Can_IdType type);
 void           Xcp_CanTxConfirmation (PduIdType XcpTxPduId);
 Std_ReturnType Xcp_CanTriggerTransmit(PduIdType XcpTxPduId, PduInfoType* PduInfoPtr);
 
-/* Eth interface callbacks */
-void           Xcp_SoAdRxIndication   (PduIdType XcpRxPduId, PduInfoType* XcpRxPduPtr);
-void           Xcp_SoAdTxConfirmation (PduIdType XcpTxPduId);
-
-
-
-#endif /* XCP_CBK_H_ */
+#endif /* XCPONCAN_CBK_H_ */
